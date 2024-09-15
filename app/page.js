7baf24cb1a65513211a6a672d1818d316de3ce58
sewkani-manya <sewkani.manya564@gmail.com>
2024-09-15 22:37:29 +0530

    set up project structure

--- app/page.js
+++ app/page.js
@@ -12,5 +12,59 @@ export default function Home() {
   ])
   const [message, setMessage]=useState('')
 
-  return <Box></Box>
+  return (<Box 
+  width="100vw" 
+  height= "100vh"
+  display="flex"
+  flexDirection="column"
+  justifyContent="center"
+  alignItems="center"
+  >
+    <Stack 
+    direction={'column'}
+    width="500px"
+    height="700px"
+    border="1px solid black"
+    p={2}
+    spacing={3}>
+      <Stack
+      direction={'column'}
+      spacing={2}
+      flexGrow={1}
+      overflow="auto"
+      maxHeight="100%">
+        {messages.map((message,index) =>(
+          <Box 
+          key={index}
+          display="flex"
+          justifyContent={
+            message.role ==='assistant' ? 'flex-start' : 'flex-ned'
+          }> 
+          <Box
+          bgcolor={
+            message.role === 'assistant'
+            ? 'primary.main'
+            : 'secondary.main'
+          }
+          color="white"
+          borderRadius={16}
+          p={3}>
+            {message.content}
+          </Box>
+          </Box>
+        ))}
+      </Stack>
+      <Stack direction={'row'} spacing={2}>
+        <TextField 
+        label="Message"
+        fullWidth
+        value={message}
+        onChange={(e) => setMessage(e.target.value)} />
+        <Button variant="contained" onClick={sendMessage}>
+          Send
+        </Button>
+      </Stack>
+    </Stack>
+  </Box>
+  )
 }
